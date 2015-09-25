4143fb3052da694674322ca5424d6e1cbcb8932d
lexaltd <lexaltd@bigmir.net>
2015-09-25 09:16:44 +0300

    str_111

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true # проверки наличия какого-нибудь содержимого во всех текстовых полях перед записью строки в базу данных
+  validates :price, numericality: {greater_than_or_equal_to: 0.01} # (больше чем или равно) значение 0.01:
+  validates :title, uniqueness: true # уникальное название.
+  validates :image_url, allow_blank: true, format: {# во избежание получения нескольких сообщений об ошибках при пустом поле используется параметр allow_blank.
+            with: %r{\.(gif|jpg|png)\Z}i,
+            message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+          } # проверим, что URL-адрес заканчивается одним из расширений: .gif, .jpg или .png.
 end
