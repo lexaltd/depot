0ecbcfdb9c72f04f759cec9e52eb2d7a7d248166
lexaltd <lexaltd@bigmir.net>
2015-11-15 12:49:52 +0200

    str_214

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  validates :name, presence: true, uniqueness: true #проверяем наличие и уникальность имени
+  has_secure_password #он заставляет Rails проверить совпадение двух паролей. Эта строка была добавлена потому, что при создании временной платформы вы указали password:digest.
 end
