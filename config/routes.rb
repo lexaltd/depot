a559bab316de3c98c9a05bfef3c30cba18a49726
lexaltd <lexaltd@bigmir.net>
2015-11-17 09:20:46 +0200

    str_219

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
-Rails.application.routes.draw do
-  get 'admin/index'
-
-  get 'sessions/new'
+Rails.application.routes.draw do 
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   get 'sessions/create'
-
   get 'sessions/destroy'
 
   resources :users
